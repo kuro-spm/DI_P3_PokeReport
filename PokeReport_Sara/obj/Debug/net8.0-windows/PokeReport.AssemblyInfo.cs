855afc5decdbe0a3a1f30bb9add2df593ae86755
kuro-spm <sarapratsm@gmail.com>
2026-01-29 12:42:01 +0100

    gitignore

--- PokeReport_Sara/obj/Debug/net8.0-windows/PokeReport.AssemblyInfo.cs
+++ PokeReport_Sara/obj/Debug/net8.0-windows/PokeReport.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PokeReport")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7cdc9eacad00d6061733692b46d39e225c9150f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("PokeReport")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PokeReport")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
